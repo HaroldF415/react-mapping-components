ac5c0c3986b96f03361629487284d27bcb9b661a
Harold F <harold.f415@gmail.com>
2020-07-27 14:17:44 -0400

    challenge complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 
+import Term from "./Term";
+import emojis from "../emojipedia";
+
+function createTerm(emoji) {
+  return (
+    <Term
+      key={emoji.id}
+      name={emoji.name}
+      emoji={emoji.emoji}
+      meaning={emoji.meaning}
+    />
+  );
+}
+
 function App() {
   return (
     <div>
@@ -7,46 +21,7 @@ function App() {
         <span>emojipedia</span>
       </h1>
 
-      <dl className="dictionary">
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              💪
-            </span>
-            <span>Tense Biceps</span>
-          </dt>
-          <dd>
-            “You can do that!” or “I feel strong!” Arm with tense biceps. Also
-            used in connection with doing sports, e.g. at the gym.
-          </dd>
-        </div>
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              🙏
-            </span>
-            <span>Person With Folded Hands</span>
-          </dt>
-          <dd>
-            Two hands pressed together. Is currently very introverted, saying a
-            prayer, or hoping for enlightenment. Is also used as a “high five”
-            or to say thank you.
-          </dd>
-        </div>
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              🤣
-            </span>
-            <span>Rolling On The Floor, Laughing</span>
-          </dt>
-          <dd>
-            This is funny! A smiley face, rolling on the floor, laughing. The
-            face is laughing boundlessly. The emoji version of “rofl“. Stands
-            for „rolling on the floor, laughing“.
-          </dd>
-        </div>
-      </dl>
+      <dl className="dictionary">{emojis.map(createTerm)}</dl>
     </div>
   );
 }
